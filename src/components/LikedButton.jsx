1ee5597552f6e2ddc2ccbf39f90d5442c9308ee9
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-06 20:09:32 +0900

    fixed css for be same line

--- src/components/LikedButton.jsx
+++ src/components/LikedButton.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 const LikedButton = styled.button`
-  color: ${props => props.color};
+  color: ${(props) => props.color};
   outline: none;
+  display: inline;
 `;
 
 export default LikedButton;
