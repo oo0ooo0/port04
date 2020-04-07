92775644c0872b1849be8061197bd9df2c930c34
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-07 22:38:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Home from './Home';
 import About from './About';
 import Works from './Works';
 import WorkDetail from './components/WorkDetail';
