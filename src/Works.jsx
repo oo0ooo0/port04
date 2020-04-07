9d752ede8127e2c51e320610a2fa20a42a281598
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-07 14:40:00 +0900

    Update Works.jsx
    changed css
    removed info text

--- src/Works.jsx
+++ src/Works.jsx
@@ -6,28 +6,32 @@ import { useSelector } from 'react-redux';
 
 const StyledWorks = styled.main`
   .my-masonry-grid {
+    margin-top: 3 0px;
     display: flex;
-    margin-left: -30px; /* gutter size offset */
+    margin-left: -20px; /* gutter size offset */
     width: auto;
   }
   .my-masonry-grid_column {
-    padding-left: 30px; /* gutter size */
+    padding-left: 20px; /* gutter size */
     background-clip: padding-box;
   }
 
   .my-masonry-grid_column > div {
     /* change div to reference your elements you put in <Masonry> */
     background: none;
-    margin-bottom: 200px;
+    margin-bottom: 20px;
   }
 `;
 
 function Works() {
-  const works = useSelector(state => state.works);
+  const works = useSelector((state) => state.works);
   return (
     <StyledWorks>
       <h2 className='title'>Work</h2>
-      <h3>최신 패션 뉴스를 전해드립니다.</h3>
+      <h3>
+        <br />
+        <br />
+      </h3>
       <Masonry
         breakpointCols={3}
         className='my-masonry-grid'
@@ -35,7 +39,7 @@ function Works() {
       >
         {Object.keys(works)
           .reverse()
-          .map(key => {
+          .map((key) => {
             return <Item key={key} {...works[key]} />;
           })}
       </Masonry>
